a5a1a505ef4a2e452539b28c1674d09aa33608ff
klaadotus <tim@truthandwhitelies.com>
2013-03-18 16:39:34 -0400

    First commit.  Devise and Bootstrap are already installed.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '56742c5fd8399cc19d2ee866fd632b80a8111e168cccf9059e26c3848b35265ccdd3574ebcddec56035fce117a74a205bbf22ac91ed7e1c872534549f3699d86'
